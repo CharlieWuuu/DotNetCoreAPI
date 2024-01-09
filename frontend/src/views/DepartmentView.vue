047a2a5258b3d18ebfac3c7715016e5c299093b2
Charlie Wu <charliewu500@gmail.com>
2024-01-09 22:37:40 +0800

    [add] 補齊前端後半的 Filter

--- frontend/src/views/DepartmentView.vue
+++ frontend/src/views/DepartmentView.vue
@@ -5,8 +5,42 @@
         <table class="table table-striped">
             <thead>
                 <tr>
-                    <th>Department Id</th>
-                    <th>Department Name</th>
+                    <th>
+                        <div class="d-flex flex-row">
+                            <input type="text" class="form-control m-2" v-model="DepartmentIdFilter" v-on:keyup="FilterFn()" placeholder="Filter" />
+
+                            <button type="button" class="btn btn-light" @click="sortResult('DepartmentId', true)">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z" />
+                                </svg>
+                            </button>
+
+                            <button type="button" class="btn btn-light" @click="sortResult('DepartmentId', false)">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z" />
+                                </svg>
+                            </button>
+                        </div>
+                        Department Id
+                    </th>
+                    <th>
+                        <div class="d-flex flex-row">
+                            <input type="text" class="form-control m-2" v-model="DepartmentIdFilter" v-on:keyup="FilterFn()" placeholder="Filter" />
+
+                            <button type="button" class="btn btn-light" @click="sortResult('DepartmentName', true)">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z" />
+                                </svg>
+                            </button>
+
+                            <button type="button" class="btn btn-light" @click="sortResult('DepartmentName', false)">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z" />
+                                </svg>
+                            </button>
+                        </div>
+                        Department Name
+                    </th>
                     <th>Options</th>
                 </tr>
             </thead>
@@ -64,10 +98,14 @@ export default {
         const modalTitle = ref('');
         const DepartmentName = ref('');
         const DepartmentId = ref(0);
+        const DepartmentNameFilter = ref('');
+        const DepartmentIdFilter = ref('');
+        const DepartmentWithoutFilter = ref([]);
 
         const refreshData = () => {
             axios.get(variables.API_URL + 'department').then((response) => {
                 departments.value = response.data;
+                DepartmentWithoutFilter.value = response.data;
             });
         };
 
@@ -112,6 +150,26 @@ export default {
             });
         };
 
+        const FilterFn = () => {
+            DepartmentNameFilter;
+            var DepartmentIdFilter = DepartmentIdFilter.value;
+            var DepartmentNameFilter = DepartmentNameFilter.value;
+
+            departments.value = DepartmentWithoutFilter.value.filter(function (el) {
+                return el.DepartmentId.toString().toLowerCase().includes(DepartmentIdFilter.toString().trim().toLowerCase()) && el.DepartmentName.toString().toLowerCase().includes(DepartmentNameFilter.toString().trim().toLowerCase());
+            });
+        };
+
+        const sortResult = (prop, asc) => {
+            departments.value = DepartmentWithoutFilter.value.sort(function (a, b) {
+                if (asc) {
+                    return a[prop] > b[prop] ? 1 : a[prop] < b[prop] ? -1 : 0;
+                } else {
+                    return b[prop] > a[prop] ? 1 : b[prop] < a[prop] ? -1 : 0;
+                }
+            });
+        };
+
         // 使用 onMounted 替代 Vue 2 的 created 鉤子
         onMounted(() => {
             refreshData();
@@ -123,12 +181,17 @@ export default {
             modalTitle,
             DepartmentName,
             DepartmentId,
+            DepartmentNameFilter,
+            DepartmentIdFilter,
+            DepartmentWithoutFilter,
             refreshData,
             addClick,
             editClick,
             createClick,
             updateClick,
             deleteClick,
+            FilterFn,
+            sortResult,
         };
     },
 };
